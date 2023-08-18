e23c4cde526424fdc1ca1d41a172685b6a857bec
helge <helge1991@hotmail.de>
2023-08-18 15:38:23 +0200

    delete button

--- streamlit_app_code/app.py
+++ streamlit_app_code/app.py
@@ -13,31 +13,58 @@ import os
 os.environ["PAFY_BACKEND"] = "internal"
 import pafy
 
-
 # Sidebar
 st.title("Object Detection using YOLOv8")
 
-st.sidebar.header("ML Model Config")
+# st.sidebar.header("Select your detection task here")
 
-mlmodel_radio = st.sidebar.radio(
-    "Select Task", ['Detection', 'Segmentation'])
-conf = float(st.sidebar.slider("Select Model Confidence", 25, 100, 40)) / 100
+# Set the default values
+mlmodel_radio = 'Detection'
+conf = 0.5  # Minimum accuracy score fixed at 50%
+
+# Display the default task and description
+# st.sidebar.write("**Task:** Detection")
+# st.sidebar.write("Perform object detection on images.")
+# st.sidebar.write("---")
+
+# Load model based on the selected task
 if mlmodel_radio == 'Detection':
     dirpath_locator = settings.DETECT_LOCATOR
     model_path = Path(settings.DETECTION_MODEL)
 elif mlmodel_radio == 'Segmentation':
     dirpath_locator = settings.SEGMENT_LOCATOR
     model_path = Path(settings.SEGMENTATION_MODEL)
+
 try:
     model = helper.load_model(model_path)
 except Exception as ex:
     print(ex)
     st.write(f"Unable to load model. Check the specified path: {model_path}")
 
+# # Sidebar
+# st.title("Object Detection using YOLOv8")
+
+# st.sidebar.header("Select your detection task here")
+
+# mlmodel_radio = st.sidebar.radio(
+#     "Select Task", ['Detection'])
+# conf = float(st.sidebar.slider("Select Minimum Accuracy Score. Recommended: Minimum 50%", 25, 100, 40)) / 100
+# if mlmodel_radio == 'Detection':
+#     dirpath_locator = settings.DETECT_LOCATOR
+#     model_path = Path(settings.DETECTION_MODEL)
+# elif mlmodel_radio == 'Segmentation':
+#     dirpath_locator = settings.SEGMENT_LOCATOR
+#     model_path = Path(settings.SEGMENTATION_MODEL)
+# try:
+#     model = helper.load_model(model_path)
+# except Exception as ex:
+#     print(ex)
+#     st.write(f"Unable to load model. Check the specified path: {model_path}")
+
 source_img = None
-st.sidebar.header("Image/Video Config")
+st.sidebar.header("Source")
 source_radio = st.sidebar.radio(
-    "Select Source", settings.SOURCES_LIST)
+    "Select the source you want to detect", settings.SOURCES_LIST)
 
 # body
 # If image is selected
@@ -112,20 +139,20 @@ elif source_radio == settings.VIDEO:
 
 elif source_radio == settings.WEBCAM:
     source_webcam = settings.WEBCAM_PATH
-    if st.sidebar.button('Detect Objects'):
-        vid_cap = cv2.VideoCapture(source_webcam)
-        stframe = st.empty()
-        while (vid_cap.isOpened()):
-            success, image = vid_cap.read()
-            if success:
-                image = cv2.resize(image, (720, int(720*(9/16))))
-                res = model.predict(image, conf=conf)
-                res_plotted = res[0].plot()
-                stframe.image(res_plotted,
-                              caption='Detected Video',
-                              channels="BGR",
-                              use_column_width=True
-                              )
+    #if st.sidebar.button('Detect Objects'):
+    vid_cap = cv2.VideoCapture(source_webcam)
+    stframe = st.empty()
+    while (vid_cap.isOpened()):
+        success, image = vid_cap.read()
+        if success:
+            image = cv2.resize(image, (720, int(720*(9/16))))
+            res = model.predict(image, conf=conf)
+            res_plotted = res[0].plot()
+            stframe.image(res_plotted,
+                            caption='Detected Video',
+                            channels="BGR",
+                            use_column_width=True
+                            )
 
 elif source_radio == settings.RTSP:
     source_rtsp = st.sidebar.text_input("rtsp stream url")
@@ -162,3 +189,100 @@ elif source_radio == settings.YOUTUBE:
                               channels="BGR",
                               use_column_width=True
                               )
+
+
+
+# from pathlib import Path
+# import PIL
+
+# import streamlit as st
+# import torch
+# import cv2
+# import pafy
+
+# import settings
+# import helper
+
+# import os
+# os.environ["PAFY_BACKEND"] = "internal"
+# import pafy
+
+# # Sidebar
+# st.title("Object Detection using YOLOv8")
+
+# # Set the default values
+# mlmodel_radio = 'Detection'
+# conf = 0.5  # Minimum accuracy score fixed at 50%
+
+# # Sidebar
+# source_img = None
+# st.sidebar.header("Source")
+# source_radio = st.sidebar.radio(
+#     "Select the source you want to detect", settings.SOURCES_LIST)
+
+# # body
+# # If image is selected
+# if source_radio == settings.IMAGE:
+#     source_img = st.sidebar.file_uploader(
+#         "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+#     save_radio = st.sidebar.radio("Save image to download", ["Yes", "No"])
+#     save = True if save_radio == 'Yes' else False
+#     col1, col2 = st.columns(2)
+
+#     with col1:
+#         if source_img is None:
+#             default_image_path = str(settings.DEFAULT_IMAGE)
+#             image = PIL.Image.open(default_image_path)
+#             st.image(default_image_path, caption='Default Image',
+#                      use_column_width=True)
+#         else:
+#             image = PIL.Image.open(source_img)
+#             st.image(source_img, caption='Uploaded Image',
+#                      use_column_width=True)
+
+#     with col2:
+#         if source_img is None:
+#             default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
+#             image = PIL.Image.open(default_detected_image_path)
+#             st.image(default_detected_image_path, caption='Detected Image',
+#                      use_column_width=True)
+#         else:
+#             if st.sidebar.button('Detect Objects'):
+#                 with torch.no_grad():
+#                     res = model.predict(
+#                         image, save=save, save_txt=save, exist_ok=True, conf=conf)
+#                     boxes = res[0].boxes
+#                     res_plotted = res[0].plot()[:, :, ::-1]
+#                     st.image(res_plotted, caption='Detected Image',
+#                              use_column_width=True)
+#                     IMAGE_DOWNLOAD_PATH = f"runs/{dirpath_locator}/predict/image0.jpg"
+#                     with open(IMAGE_DOWNLOAD_PATH, 'rb') as fl:
+#                         st.download_button("Download object-detected image",
+#                                            data=fl,
+#                                            file_name="image0.jpg",
+#                                            mime='image/jpg'
+#                                            )
+#                 try:
+#                     with st.expander("Detection Results"):
+#                         for box in boxes:
+#                             st.write(box.xywh)
+#                 except Exception as ex:
+#                     # st.write(ex)
+#                     st.write("No image is uploaded yet!")
+
+# elif source_radio == settings.WEBCAM:
+#     source_webcam = settings.WEBCAM_PATH
+#     if st.sidebar.button('Detect Objects'):
+#         vid_cap = cv2.VideoCapture(source_webcam)
+#         stframe = st.empty()
+#         while (vid_cap.isOpened()):
+#             success, image = vid_cap.read()
+#             if success:
+#                 image = cv2.resize(image, (720, int(720*(9/16))))
+#                 res = model.predict(image, conf=conf)
+#                 res_plotted = res[0].plot()
+#                 stframe.image(res_plotted,
+#                               caption='Detected Video',
+#                               channels="BGR",
+#                               use_column_width=True
+#                               )
