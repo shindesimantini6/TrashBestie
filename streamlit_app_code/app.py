b360ba05a2d14a14c4597bb8181334928a5aaed4
helge <helge1991@hotmail.de>
2023-08-18 18:19:54 +0200

    background color added

--- streamlit_app_code/app.py
+++ streamlit_app_code/app.py
@@ -14,7 +14,7 @@ os.environ["PAFY_BACKEND"] = "internal"
 import pafy
 
 # Sidebar
-st.title("Object Detection using YOLOv8")
+st.title("Garbige Detection & assorting help using YOLOv8")
 
 # Set the default values
 mlmodel_radio = 'Detection'
@@ -35,25 +35,56 @@ except Exception as ex:
     st.write(f"Unable to load model. Check the specified path: {model_path}")
 
 source_img = None
-st.sidebar.header("Source")
-source_radio = st.sidebar.radio(
-    "Select the source you want to detect", settings.SOURCES_LIST)
+
+# Add a description to the "Information" header
+st.sidebar.header("Information")
+st.sidebar.write("This section provides additional information about the application.")
+
+# Categories with indentation using Markdown
+st.sidebar.write("Aluminium can: This garbage has to be sorted XXX",
+                 "\n\n",
+                 "Battery: XXX"
+                 "\n\n",
+                 "Pen: XXX",
+                 "\n\n",
+                 "Leftovers: XXX",
+                 "\n\n",
+                 "Toothbrushes: XXX",
+                 "\n\n")
+                 
+# st.sidebar.write("Aluminium can: This garbage has to be sorted XXX", 
+#                  "Battery: XXX",
+#                  "Pen: XXX",
+#                  "Leftovers: XXX",
+#                  "Toothbrushes: XXX")
+# source_radio = st.sidebar.radio(
+#     "Information", settings.SOURCES_LIST)
 
 # body
 
-if source_radio == settings.WEBCAM:
-    source_webcam = settings.WEBCAM_PATH
-    #if st.sidebar.button('Detect Objects'):
-    vid_cap = cv2.VideoCapture(source_webcam)
-    stframe = st.empty()
-    while (vid_cap.isOpened()):
-        success, image = vid_cap.read()
-        if success:
-            image = cv2.resize(image, (720, int(720*(9/16))))
-            res = model.predict(image, conf=conf)
-            res_plotted = res[0].plot()
-            stframe.image(res_plotted,
-                            caption='Detected Video',
-                            channels="BGR",
-                            use_column_width=True
-                            )
+
+source_webcam = settings.WEBCAM_PATH
+#if st.sidebar.button('Detect Objects'):
+vid_cap = cv2.VideoCapture(source_webcam)
+stframe = st.empty()
+while (vid_cap.isOpened()):
+    success, image = vid_cap.read()
+    if success:
+        image = cv2.resize(image, (720, int(720*(9/16))))
+        res = model.predict(image, conf=conf)
+        res_plotted = res[0].plot()
+        stframe.image(res_plotted,
+                        caption='Detected Video',
+                        channels="BGR",
+                        use_column_width=True
+                        )
+        
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #252525
+    }
+
+    """
+)
