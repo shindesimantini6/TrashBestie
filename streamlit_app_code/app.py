a303145e3d9b2b95fae2eb839d8e5a76117d7565
helge <helge1991@hotmail.de>
2023-08-18 15:59:49 +0200

    deleted image feature

--- streamlit_app_code/app.py
+++ streamlit_app_code/app.py
@@ -40,55 +40,8 @@ source_radio = st.sidebar.radio(
     "Select the source you want to detect", settings.SOURCES_LIST)
 
 # body
-if source_radio == settings.IMAGE:
-    source_img = st.sidebar.file_uploader(
-        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
-    save_radio = st.sidebar.radio("Save image to download", ["Yes", "No"])
-    save = True if save_radio == 'Yes' else False
-    col1, col2 = st.columns(2)
 
-    with col1:
-        if source_img is None:
-            default_image_path = str(settings.DEFAULT_IMAGE)
-            image = PIL.Image.open(default_image_path)
-            st.image(default_image_path, caption='Default Image',
-                     use_column_width=True)
-        else:
-            image = PIL.Image.open(source_img)
-            st.image(source_img, caption='Uploaded Image',
-                     use_column_width=True)
-
-    with col2:
-        if source_img is None:
-            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
-            image = PIL.Image.open(default_detected_image_path)
-            st.image(default_detected_image_path, caption='Detected Image',
-                     use_column_width=True)
-        else:
-            if st.sidebar.button('Detect Objects'):
-                with torch.no_grad():
-                    res = model.predict(
-                        image, save=save, save_txt=save, exist_ok=True, conf=conf)
-                    boxes = res[0].boxes
-                    res_plotted = res[0].plot()[:, :, ::-1]
-                    st.image(res_plotted, caption='Detected Image',
-                             use_column_width=True)
-                    IMAGE_DOWNLOAD_PATH = f"runs/{dirpath_locator}/predict/image0.jpg"
-                    with open(IMAGE_DOWNLOAD_PATH, 'rb') as fl:
-                        st.download_button("Download object-detected image",
-                                           data=fl,
-                                           file_name="image0.jpg",
-                                           mime='image/jpg'
-                                           )
-                try:
-                    with st.expander("Detection Results"):
-                        for box in boxes:
-                            st.write(box.xywh)
-                except Exception as ex:
-                    # st.write(ex)
-                    st.write("No image is uploaded yet!")
-
-elif source_radio == settings.WEBCAM:
+if source_radio == settings.WEBCAM:
     source_webcam = settings.WEBCAM_PATH
     #if st.sidebar.button('Detect Objects'):
     vid_cap = cv2.VideoCapture(source_webcam)
