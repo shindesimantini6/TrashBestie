0ee4fddbc4cce93912e664a539504ec48ce4bb30
shinde <shinde@gfz-potsdam.de>
2023-08-18 12:30:09 +0200

    Added streamlit code

---
+++ streamlit_app_code/app.py
@@ -0,0 +1,164 @@
+from pathlib import Path
+import PIL
+
+import streamlit as st
+import torch
+import cv2
+import pafy
+
+import settings
+import helper
+
+import os
+os.environ["PAFY_BACKEND"] = "internal"
+import pafy
+
+
+# Sidebar
+st.title("Object Detection using YOLOv8")
+
+st.sidebar.header("ML Model Config")
+
+mlmodel_radio = st.sidebar.radio(
+    "Select Task", ['Detection', 'Segmentation'])
+conf = float(st.sidebar.slider("Select Model Confidence", 25, 100, 40)) / 100
+if mlmodel_radio == 'Detection':
+    dirpath_locator = settings.DETECT_LOCATOR
+    model_path = Path(settings.DETECTION_MODEL)
+elif mlmodel_radio == 'Segmentation':
+    dirpath_locator = settings.SEGMENT_LOCATOR
+    model_path = Path(settings.SEGMENTATION_MODEL)
+try:
+    model = helper.load_model(model_path)
+except Exception as ex:
+    print(ex)
+    st.write(f"Unable to load model. Check the specified path: {model_path}")
+
+source_img = None
+st.sidebar.header("Image/Video Config")
+source_radio = st.sidebar.radio(
+    "Select Source", settings.SOURCES_LIST)
+
+# body
+# If image is selected
+if source_radio == settings.IMAGE:
+    source_img = st.sidebar.file_uploader(
+        "Choose an image...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+    save_radio = st.sidebar.radio("Save image to download", ["Yes", "No"])
+    save = True if save_radio == 'Yes' else False
+    col1, col2 = st.columns(2)
+
+    with col1:
+        if source_img is None:
+            default_image_path = str(settings.DEFAULT_IMAGE)
+            image = PIL.Image.open(default_image_path)
+            st.image(default_image_path, caption='Default Image',
+                     use_column_width=True)
+        else:
+            image = PIL.Image.open(source_img)
+            st.image(source_img, caption='Uploaded Image',
+                     use_column_width=True)
+
+    with col2:
+        if source_img is None:
+            default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
+            image = PIL.Image.open(default_detected_image_path)
+            st.image(default_detected_image_path, caption='Detected Image',
+                     use_column_width=True)
+        else:
+            if st.sidebar.button('Detect Objects'):
+                with torch.no_grad():
+                    res = model.predict(
+                        image, save=save, save_txt=save, exist_ok=True, conf=conf)
+                    boxes = res[0].boxes
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption='Detected Image',
+                             use_column_width=True)
+                    IMAGE_DOWNLOAD_PATH = f"runs/{dirpath_locator}/predict/image0.jpg"
+                    with open(IMAGE_DOWNLOAD_PATH, 'rb') as fl:
+                        st.download_button("Download object-detected image",
+                                           data=fl,
+                                           file_name="image0.jpg",
+                                           mime='image/jpg'
+                                           )
+                try:
+                    with st.expander("Detection Results"):
+                        for box in boxes:
+                            st.write(box.xywh)
+                except Exception as ex:
+                    # st.write(ex)
+                    st.write("No image is uploaded yet!")
+
+elif source_radio == settings.VIDEO:
+    source_vid = st.sidebar.selectbox(
+        "Choose a video...", settings.VIDEOS_DICT.keys())
+    video_file = open(settings.VIDEOS_DICT.get(source_vid), 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
+    if st.sidebar.button('Detect Video Objects'):
+        vid_cap = cv2.VideoCapture(str(settings.VIDEOS_DICT.get(source_vid)))
+        stframe = st.empty()
+        while (vid_cap.isOpened()):
+            success, image = vid_cap.read()
+            if success:
+                image = cv2.resize(image, (720, int(720*(9/16))))
+                res = model.predict(image, conf=conf)
+                res_plotted = res[0].plot()
+                stframe.image(res_plotted,
+                              caption='Detected Video',
+                              channels="BGR",
+                              use_column_width=True
+                              )
+
+elif source_radio == settings.WEBCAM:
+    source_webcam = settings.WEBCAM_PATH
+    if st.sidebar.button('Detect Objects'):
+        vid_cap = cv2.VideoCapture(source_webcam)
+        stframe = st.empty()
+        while (vid_cap.isOpened()):
+            success, image = vid_cap.read()
+            if success:
+                image = cv2.resize(image, (720, int(720*(9/16))))
+                res = model.predict(image, conf=conf)
+                res_plotted = res[0].plot()
+                stframe.image(res_plotted,
+                              caption='Detected Video',
+                              channels="BGR",
+                              use_column_width=True
+                              )
+
+elif source_radio == settings.RTSP:
+    source_rtsp = st.sidebar.text_input("rtsp stream url")
+    if st.sidebar.button('Detect Objects'):
+        vid_cap = cv2.VideoCapture(source_rtsp)
+        stframe = st.empty()
+        while (vid_cap.isOpened()):
+            success, image = vid_cap.read()
+            if success:
+                image = cv2.resize(image, (720, int(720*(9/16))))
+                res = model.predict(image, conf=conf)
+                res_plotted = res[0].plot()
+                stframe.image(res_plotted,
+                              caption='Detected Video',
+                              channels="BGR",
+                              use_column_width=True
+                              )
+
+elif source_radio == settings.YOUTUBE:
+    source_youtube = st.sidebar.text_input("YouTube Video url")
+    if st.sidebar.button('Detect Objects'):
+        video = pafy.new(source_youtube)
+        best = video.getbest(preftype="mp4")
+        cap = cv2.VideoCapture(best.url)
+        stframe = st.empty()
+        while (cap.isOpened()):
+            success, image = cap.read()
+            if success:
+                image = cv2.resize(image, (720, int(720*(9/16))))
+                res = model.predict(image, conf=conf)
+                res_plotted = res[0].plot()
+                stframe.image(res_plotted,
+                              caption='Detected Video',
+                              channels="BGR",
+                              use_column_width=True
+                              )
---
+++ streamlit_app_code/settings.py
@@ -0,0 +1,102 @@
+from pathlib import Path
+import sys
+import cv2
+
+# Get the absolute path of the current file
+file_path = Path(__file__).resolve()
+
+# Get the parent directory of the current file
+root_path = file_path.parent
+
+# Add the root path to the sys.path list if it is not already there
+if root_path not in sys.path:
+    sys.path.append(str(root_path))
+
+# Get the relative path of the root directory with respect to the current working directory
+ROOT = root_path.relative_to(Path.cwd())
+
+# Source
+IMAGE = 'Image'
+VIDEO = 'Video'
+WEBCAM = 'Webcam'
+RTSP = 'RTSP'
+YOUTUBE = 'YouTube'
+
+SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, RTSP, YOUTUBE]
+
+# images
+IMAGES_DIR = ROOT / 'images'
+DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
+
+# video
+VIDEO_DIR = ROOT / 'videos'
+VIDEO_1_PATH = VIDEO_DIR / 'king_bolete.mp4'
+VIDEO_2_PATH = VIDEO_DIR / 'king_bolete_2.mp4'
+VIDEO_3_PATH = VIDEO_DIR / 'King_bolete_final_project.mp4'
+VIDEO_4_PATH = VIDEO_DIR / 'king_bolete_3.mp4'
+VIDEO_5_PATH = VIDEO_DIR / 'king_bolete_4.mp4'
+VIDEOS_DICT = {
+    'video_1': VIDEO_1_PATH,
+    'video_2': VIDEO_2_PATH,
+    'video_3': VIDEO_3_PATH,
+    'video_4': VIDEO_4_PATH,
+    'video_5': VIDEO_5_PATH,
+}
+
+# VIDEOS_DICT = {
+#     'video_1': VIDEO_1_PATH,
+#     'video_2': VIDEO_2_PATH,
+#     'video_3': VIDEO_3_PATH,
+#     'video_4': VIDEO_4_PATH,
+# }
+
+# model
+MODEL_DIR = ROOT / 'weights'
+DETECTION_MODEL = MODEL_DIR / 'best.pt'
+SEGMENTATION_MODEL = MODEL_DIR / 'yolov8n-seg.pt'
+
+
+# Detected/segmented image dirpath locator
+DETECT_LOCATOR = 'detect'
+SEGMENT_LOCATOR = 'segment'
+
+
+# Webcam
+WEBCAM_PATH = 0
+
+
+# def process_video(input_video_path, output_video_path):
+#     cap = cv2.VideoCapture(str(input_video_path))
+
+#     # Get the video frame rate
+#     fps = cap.get(cv2.CAP_PROP_FPS)
+
+#     # Define the codec for the output video
+#     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+#     # Get the size of the video frame
+#     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+#     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+#     # Create the VideoWriter object
+#     out = cv2.VideoWriter(str(output_video_path), fourcc, fps, (width, height))
+
+#     while cap.isOpened():
+#         ret, frame = cap.read()
+#         if not ret:
+#             break
+
+#         # Process the frame here
+#         processed_frame = frame  # Replace this line with your own code to process the frame
+
+#         # Write the processed frame to the output video
+#         out.write(processed_frame)
+
+#     cap.release()
+#     out.release()
+
+# # Process the video and save the output to a file
+# input_video_path = VIDEOS_DICT['video_1']
+# output_video_path = VIDEO_DIR / 'king_bolete_processed.mp4'
+# process_video(input_video_path, output_video_path)
