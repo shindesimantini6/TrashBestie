ffb46387e7159edab7702ebd32f073e0feaf1942
Minh <vanmy@hotmail.de>
2023-08-21 02:18:09 +0200

    adjust information

--- streamlit_app_code/class_description.py
+++ streamlit_app_code/class_description.py
@@ -1,13 +1,12 @@
-
-
-class_descriptions = {'aluminum-cans': {'waste_bin' : 'Waste Bin = YELLOW', 'description': 
-                                        'Aluminium cans are recylcable and hence they go into Yellow'}, 
-'battery' : {'waste_bin' : 'Waste Bin = BSR or DM or Penny or Rossmann', 'description': 
-                                        'Batteries are specially recyled and have to be dropped on at one of the pick up locations of BSR'},
-'leftovers': {'waste_bin' : 'Waste Bin = BIO (Organic)', 'description': 
-                                        'Leftover food waste can be decomposed.'},
-'pen' : {'waste_bin' : 'Waste Bin = YELLOW', 'description': 
-                                        'Pens can be recycled.'},
-'toothbrushes': {'waste_bin' : 'Waste Bin = BLACK', 'description': 
-                                        'Unsure why.'}
-                                        }
+class_descriptions = {
+    'aluminum-cans': {'waste_bin' : 'Waste Bin = YELLOW', 
+                      'description': 'Aluminum cans are recyclable, and as such, they should be placed in the yellow bin.'}, 
+    'battery' : {'waste_bin' : 'Waste Bin = BSR or at retailers, that sell batteries', 
+                 'description': 'Batteries can contain hazardous heavy metals such as lead, cadmium, or mercury. That is why they need to be collected separately. Consumers are legally obliged to return used batteries and accumulators to collection points, for example, at retailers (anywhere batteries are sold) or at public waste disposal facilities like recycling centers and hazardous waste collection points.'},
+    'leftovers': {'waste_bin' : 'Waste Bin = BIO (Organic)', 
+              'description': 'In the bio-bin, place all organic waste. This helps conserve the environment, climate, and your wallet. Biowaste is turned into compost and biogas, saving over 9,000 tons of CO2 annually. The produced biogas replaces 2.5 million liters of diesel each year, and the residue aids in reducing the use of chemical fertilizers.'},
+    'pen' : {'waste_bin' : 'Waste Bin = BLACK',
+             'description': 'Ballpoint pens are not packaging and should not be placed in the Yellow Bin. Ballpoint pens are residual waste and should be disposed of through your regular household waste bin.'},
+    'toothbrushes': {'waste_bin' : 'Waste Bin = BLACK',
+                     'description': 'As toothbrushes are disposable items and not considered as packaging, they should be disposed of in the regular black bin and not in the yellow bag.'}
+    }
