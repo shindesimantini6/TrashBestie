f92b421a4ff39cc82b2e2c1847ec1e13eeee9eaf
Simantini Shinde <79316344+shindesimantini6@users.noreply.github.com>
2023-08-23 11:00:26 +0200

    Update Home.py

--- streamlit_app_code/Home.py
+++ streamlit_app_code/Home.py
@@ -9,27 +9,18 @@ st.write("# Welcome to TrashBestie! 🗑️")
 
 st.markdown(
     """
-    This is TrashBestie, your app to sort waste and reach the full potential of waste recycling.
+This is TrashBestie, your app to sort waste and reach the full potential of waste recycling.
+Whether you're passionate about recycling, waste reduction, or simply curious about what's considered garbage, 
+our Real-Time Garbage Object Detection app is a fun and educational way to contribute to a cleaner world.
 
-    ##Join Us in Keeping the Environment Clean
+TrashBestie scans your garbage, tells you what kind of garbage it is and which garbage bin should it be thrown into. Currently the
+trash sorting follows the sorting system from Germany. TrashBestie is trained on a Yolov8 model and is trained to identify, `aluminium cans`, `toothbrushes`, 
+`batteries` and `pens`. 
 
-Whether you're passionate about recycling, waste reduction, or simply curious about what's considered garbage, our Real-Time Garbage Object Detection app is a fun and educational way to contribute to a cleaner world.
-
-Ready to start? Click on the "Upload Image" button and see the power of AI in action as it identifies garbage objects and 
+Ready to start? Click on the "Upload Image" or "Webcam" button and see the power of AI in action as it identifies garbage objects and 
 helps you become a better waste warrior.
-Remember, every step counts toward reducing waste and preserving our environment for future generations. Let's work together for a cleaner, greener planet!
+
+Remember, every step counts toward reducing waste and preserving our environment for future generations. 
+Let's work together for a cleaner, greener planet!
 """
 )
-
-# TO DO:
-# Remove Radio buttons : DONE
-# Remove app.py : DONE
-# Fix import of settings : DONE
-# comment out download image : DONE (kept it)
-# col1 : Image and col2: description : DONE
-# Change the font of the descriptiom : DONE
-# Push it on the cloud 
-# Sub page with resources : DONE
-# Link in the description to the resources : 
-# Add a page for resources : dataset information (links) : DONE
-# add links the for yolov8 and roboflow and streamlit :DONE
